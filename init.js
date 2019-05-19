f640337daca38de8b709e9fddee468500cbac50b
Daniel Frech <dfrechdev@gmail.com>
2019-05-19 22:48:07 +0200

    corrected publish and browsersync defaults

--- init.js
+++ init.js
@@ -16,117 +16,119 @@ module.exports = init;
  * @description Entry point for creating a new app with the template
  */
 function init(appDetails) {
-    const config = {
-        libraryName: 'undefined',
-        libraryCode: 'undefined',
-        main: './src/main.js',
-        globals: {
-            apex: 'apex'
-        },
-        external: ['apex'],
-        cssExtensions: ['.css', '.less'],
-        targets: 'last 2 versions, >0.25%, not dead',
-        version: 'undefined'
-    };
-    if (appDetails.suppressInquiry) {
-        return Promise.resolve(config);
-    }
-    return inquirer.prompt(getQuestions(appDetails)).then(answers => {
-        return new Promise((resolve, reject) => {
-            try {
-                config.libraryName = answers['library-name'];
-                config.libraryCode = answers['library-code'];
-                config.version = answers['version'];
+	const config = {
+		libraryName: 'undefined',
+		libraryCode: 'undefined',
+		main: './src/main.js',
+		globals: {
+			apex: 'apex'
+		},
+		external: ['apex'],
+		cssExtensions: ['.css', '.less'],
+		targets: 'last 2 versions, >0.25%, not dead',
+		version: 'undefined'
+	};
+	if (appDetails.suppressInquiry) {
+		return Promise.resolve(config);
+	}
+	return inquirer.prompt(getQuestions(appDetails)).then(answers => {
+		return new Promise((resolve, reject) => {
+			try {
+				config.libraryName = answers['library-name'];
+				config.libraryCode = answers['library-code'];
+				config.version = answers['version'];
 
-                // browsersync settings:
-                config.browsersync.realTime = true;
-                config.browsersync.ghostMode = false;
-                config.browsersync.notify = true;
-                config.apex = {};
-                config.apex.openBuilder = false;
+				// set default browsersync settings:
+				config.browsersync = {};
+				config.browsersync.realTime = true;
+				config.browsersync.ghostMode = false;
+				config.browsersync.notify = true;
+				config.apex = {};
+				config.apex.openBuilder = false;
 
-                // publish defaults:
-                config.publish.destination = 'application';
-                config.publish.path = 'sqlcl';
-                config.publish.username = '';
-                config.publish.password = '';
-                config.publish.connectionString = '';
+				// set default publish settings:
+				config.publish = {};
+				config.publish.destination = 'application';
+				config.publish.path = 'sqlcl';
+				config.publish.username = '';
+				config.publish.password = '';
+				config.publish.connectionString = '';
 
-                resolve(config);
-            } catch (err) {
-                reject(err);
-            }
-        });
-    });
+				resolve(config);
+			} catch (err) {
+				reject(err);
+			}
+		});
+	});
 }
 
 const isRequired = function(input) {
-    if (input !== '') {
-        return true;
-    }
+	if (input !== '') {
+		return true;
+	}
 
-    return 'Required.';
+	return 'Required.';
 };
 
 /**
  * @private
  */
 function getQuestions(appDetails) {
-    return [
-        {
-            type: 'input',
-            name: 'srcFolder',
-            message: 'Location of the source folder?',
-            default: './src',
-            validate: isRequired
-        },
-        {
-            type: 'input',
-            name: 'distFolder',
-            message: 'Location of the distribution folder?',
-            default: './dist',
-            validate: isRequired,
-            when(answers) {
-                return ['advanced', 'pro'].includes(answers.mode);
-            }
-        },
-        {
-            name: 'library-name',
-            type: 'input',
-            default: appDetails.appName,
-            message: 'Library name:',
-            validate: input => {
-                if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-                else
-                    return 'The library name may only include letters, numbers, underscores and hashes.';
-            }
-        },
-        {
-            name: 'library-code',
-            type: 'input',
-            default: appDetails.appName,
-            message: 'Library code:',
-            validate: input => {
-                if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-                else
-                    return 'The library code may only include letters, numbers, underscores and hashes.';
-            }
-        },
-        {
-            name: 'initial-version',
-            type: 'input',
-            message: 'Initial version:',
-            default: '1.0.0',
-            validate: input => {
-                if (semverRegex().test(input)) return true;
-                else return 'The initial version must match a semantic versions such as 0.0.1';
-            }
-        },
-        {
-            type: 'input',
-            name: 'appURL',
-            message: 'The URL of your APEX application?',
-            validate: isRequired
-        }
-    ];
+	return [
+		{
+			type: 'input',
+			name: 'srcFolder',
+			message: 'Location of the source folder?',
+			default: './src',
+			validate: isRequired
+		},
+		{
+			type: 'input',
+			name: 'distFolder',
+			message: 'Location of the distribution folder?',
+			default: './dist',
+			validate: isRequired,
+			when(answers) {
+				return ['advanced', 'pro'].includes(answers.mode);
+			}
+		},
+		{
+			name: 'library-name',
+			type: 'input',
+			default: appDetails.appName,
+			message: 'Library name:',
+			validate: input => {
+				if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+				else
+					return 'The library name may only include letters, numbers, underscores and hashes.';
+			}
+		},
+		{
+			name: 'library-code',
+			type: 'input',
+			default: appDetails.appName,
+			message: 'Library code:',
+			validate: input => {
+				if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+				else
+					return 'The library code may only include letters, numbers, underscores and hashes.';
+			}
+		},
+		{
+			name: 'initial-version',
+			type: 'input',
+			message: 'Initial version:',
+			default: '1.0.0',
+			validate: input => {
+				if (semverRegex().test(input)) return true;
+				else return 'The initial version must match a semantic versions such as 0.0.1';
+			}
+		},
+		{
+			type: 'input',
+			name: 'appURL',
+			message: 'The URL of your APEX application?',
+			validate: isRequired
+		}
+	];
 }
