ae8e4ec70cde4d9f1afa513899bd3384802a70c9
Vincent Morneau <vincent.morneau@gmail.com>
2019-12-01 11:57:18 -0500

    dep cleanup

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-    presets: [
-        [
-            '@babel/env',
-            {
-                useBuiltIns: 'usage',
-                corejs: 3,
-            },
-        ],
-    ],
-    exclude: 'node_modules/**',
+    presets: ["@babel/preset-env"]
 };
