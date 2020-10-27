add4a712e511b5e87e46cbded8ea2e1a9baacad9
Vincent Morneau <vincent.morneau@gmail.com>
2020-10-27 15:41:24 -0400

    updating template for v5

--- template/webpack.dev.js
+++ template/webpack.dev.js
@@ -1,18 +1,17 @@
-const path = require('path');
-const apexnitroConfig = require('./apexnitro.config.json');
-const autoprefixer = require('autoprefixer');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const apexnitroConfig = require("./apexnitro.config.json");
+const CopyPlugin = require("copy-webpack-plugin");
 
 apexnitroConfig.globals = apexnitroConfig.external.reduce((a, b) => ((a[b] = b), a), {});
 
 module.exports = {
-  mode: 'development',
-  devtool: 'eval',
+  mode: "development",
+  devtool: "eval-source-map",
   entry: [apexnitroConfig.mainCss, apexnitroConfig.mainJs],
   output: {
     path: path.resolve(apexnitroConfig.distFolder),
     library: apexnitroConfig.libraryName,
-    filename: `${apexnitroConfig.libraryName}.js`,
+    filename: `${apexnitroConfig.libraryName}.js`
   },
   module: {
     rules: [
@@ -20,48 +19,59 @@ module.exports = {
         test: /\.scss$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: `${apexnitroConfig.libraryName}.css`,
-            },
+              name: `${apexnitroConfig.libraryName}.css`
+            }
           },
-          { loader: 'extract-loader' },
-          { loader: 'css-loader?-url' },
+          { loader: "extract-loader" },
+          { loader: "css-loader?-url" },
           {
             loader: 'postcss-loader',
             options: {
-              plugins: () => [autoprefixer()],
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              webpackImporter: false,
-              implementation: require('sass'),
-              sassOptions: {
-                includePaths: ['./node_modules'],
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
               },
             },
           },
-        ],
+          {
+            loader: "sass-loader",
+            options: {
+              webpackImporter: false,
+              implementation: require("sass"),
+              sassOptions: {
+                includePaths: ["./node_modules"]
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env'],
-        },
-      },
-    ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(apexnitroConfig.srcFolder, 'static'),
-          to: path.resolve(apexnitroConfig.distFolder, 'static'),
-        },
-      ],
-    }),
-  ],
+          from: path.resolve(apexnitroConfig.srcFolder, "static"),
+          to: path.resolve(apexnitroConfig.distFolder, "static")
+        }
+      ]
+    })
+  ]
 };
